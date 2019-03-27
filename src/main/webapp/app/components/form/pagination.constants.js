04aa310f4ef574cc9de05472f3042c8d1b75996f
ahat <ahatzikonstantinou@gmail.com>
2019-03-27 21:01:43 +0200

    Initial application generated by JHipster-4.10.0

---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('mmsnapWebApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
---
+++ src/main/webapp/app/services/auth/password-reset-finish.service.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('mmsnapWebApp')
+        .factory('PasswordResetFinish', PasswordResetFinish);
+
+    PasswordResetFinish.$inject = ['$resource'];
+
+    function PasswordResetFinish($resource) {
+        var service = $resource('api/account/reset-password/finish', {}, {});
+
+        return service;
+    }
+})();
